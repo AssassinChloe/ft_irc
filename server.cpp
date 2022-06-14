9bab45346de0e52e925406bca8dae3e09d97f43b
Chloe ASSASSIN <cassassi@e2r2p21.clusters.42paris.fr>
2022-06-14 15:01:49 +0200

    un peu plus propre et un peu plus cpp avec un vector au lieu d'un array malloc de struct pollfd

--- server.cpp
+++ server.cpp
@@ -6,7 +6,7 @@
 /*   By: cassassi <cassassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 16:39:38 by cassassi          #+#    #+#             */
-/*   Updated: 2022/06/14 11:57:06 by cassassi         ###   ########.fr       */
+/*   Updated: 2022/06/14 15:00:05 by cassassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,63 +14,56 @@
 
 int stop = 0;
 
+//je suis pas sure que exit soit autorise, je suis perdue avec ces histoire de fonction libC autorisees
 void ft_error(std::string str)
 {
     std::cout << str << std::endl;
     exit(1);
 }
 
-void sighandler(int sig) { 
+void sighandler(int sig)
+{ 
     if (sig == SIGINT)
         stop = 1; 
     if (sig == SIGPIPE)
         stop = 2;
-    }
+}
 
-/*
-    waaaah le beau serveeeeeur oulalala! C'EST CLAIR !!!!
-*/
 void *get_in_addr(struct sockaddr *sa)
 {
     return &(((struct sockaddr_in*)sa)->sin_addr);
 }
 
-void add_to_pfds(struct pollfd **pfds, int newfd, int *fd_count, int *fd_size)
+void add_fd(int fd, std::vector<struct pollfd> *fds)
 {
-    // If we don't have room, add more space in the pfds array
-    if (*fd_count == *fd_size) {
-        *fd_size *= 2; // Double it
-
-        *pfds = (struct pollfd *)realloc(*pfds, sizeof(**pfds) * (*fd_size));
-    }
-
-    (*pfds)[*fd_count].fd = newfd;
-    (*pfds)[*fd_count].events = POLLIN; // Check ready-to-read
-
-    (*fd_count)++;
+    struct pollfd tmp;
+    tmp.fd = fd;
+    tmp.events = POLLIN;
+    fds->push_back(tmp);
 }
 
-void del_from_pfds(struct pollfd *pfds, int i, int *fd_count)
+void delete_one(std::vector<struct pollfd> *fds, struct pollfd to_erase)
 {
-    // Copy the one from the end over this one
-    pfds[i] = pfds[*fd_count-1];
-
-    (*fd_count)--;
+    for(std::vector<struct pollfd>::iterator it = fds->begin(); it != fds->end(); it++)
+    {
+        if ((*it).fd == to_erase.fd)
+        {
+            fds->erase(it);
+            return ;
+        }
+    }
 }
 
 int get_listener_socket(char *port)
 {
-    int listener;     // Listening socket descriptor
-    int yes=1;        // For setsockopt() SO_REUSEADDR, below
+    int listener;     
+    int yes=1;        
     int ret;
-    //var pour getaddrinfo
+
     struct addrinfo hints;
     struct addrinfo *res;
     struct addrinfo *tmp;
-    // char host[256] = {0};
-    // struct in_addr translate;
-    
-    //Initialisation de addrinfo
+  
     memset(&hints, 0, sizeof(struct addrinfo));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
@@ -95,13 +88,9 @@ int get_listener_socket(char *port)
         }
         break;
     }
-    freeaddrinfo(res); // All done with this
-
-    // If we got here, it means we didn't get bound
+    freeaddrinfo(res); 
     if (tmp == NULL)
         return -1;
-
-    // Listen
     if (listen(listener, BACKLOG) == -1)
         return -1;
     return listener;
@@ -111,125 +100,72 @@ int get_listener_socket(char *port)
 int main(int argc, char **argv)
 {
     int sockfd, newfd;
-    struct sockaddr_in  localaddr;
     struct sockaddr distaddr;
-    socklen_t size = sizeof(localaddr);
+    socklen_t size;
     char buff[BUFFER_SIZE] = {0}; 
-    
-    // var pour poll()
-    struct pollfd *fds;
-    int fd_size = 5;
-    int fd_count = 0;
+    std::vector<struct pollfd> fds; // vector qui va stocker tous nos socket au fil des connexions des clients
     int nb_event;
     
-    // pour lancement du programme suivant le sujet, a voir plus tard
     if (argc != 3)
 	{
 		std::cout << "./ircserv <port> <password>" << std::endl;
 		return 1;
 	}
-    // attention pour l'instant je ne sais pas du tout ce qu'il faut faire en cas de SIGPIPE
 	signal(SIGINT, sighandler);
-    signal(SIGPIPE, sighandler);
-    
-    fds = (struct pollfd *)malloc(sizeof(*fds) * fd_size);
-    if (!fds)
-        ft_error("erreur malloc");
-    sockfd = get_listener_socket(argv[1]);
+    signal(SIGPIPE, sighandler); // attention pour l'instant je ne sais pas du tout ce qu'il faut faire en cas de SIGPIPE
+
+      
+    sockfd = get_listener_socket(argv[1]); //initialisation du socket d'ecoute sur le port passe en parametre
     if (sockfd < 0)
         ft_error("error getting a listening socket");
+    add_fd(sockfd, &fds);
     
-    //initialisation des variable de poll()
-    fds[0].fd = sockfd;
-    fds[0].events = POLLIN;
-    fd_count++;
-    
-     
-    // //l'instanciation de la struc sockaddr_in pour se connecter au port 8080 (MYPORT define dans le fichier .hpp)
-    // localaddr.sin_family = AF_INET;
-    // localaddr.sin_port = htons(MYPORT);
-    // localaddr.sin_addr.s_addr = INADDR_ANY;
-    // bzero(localaddr.sin_zero, 8);
-
-    // //on ouvre la chaussette    
-    // sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    // if  (sockfd < 0)
-    //     ft_error("error socket()");
-    
-    // //on y mets des option auxquelles je comprends rien
-    // if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
-    //     ft_error("error setsockopt()");
-    
-    // //on bind la socket avec la struct localaddr pour que lefd sache avec quel port il bosse
-    // if (bind(sockfd, (struct sockaddr *)&localaddr, size) < 0)
-    //     ft_error("error bind()");
-
-    // //on ecoute avec une file d'attente == BACKLOG
-    // if (listen(sockfd, BACKLOG) < 0)
-    //     ft_error("error listen()");
-    
-    //on accept la connexion entrante et on la stocke dans distaddr
-    // if ((newfd = accept(sockfd, (struct sockaddr *)&distaddr, &size)) < 0)
-    //             ft_error("error accept()");
-    
-   
     //boucle server    
     while (stop == 0)
     {   
-        //tentative mais paul et moi on s'entend pas trop  
-        nb_event = poll(fds, fd_count, -1);
+        nb_event = poll(&fds[0], fds.size(), -1);
         if (nb_event < 0)
         {
-            perror("poll");
-            ft_error("");
+            if (stop != 0)
+                break ;
+            else
+                ft_error("error poll");
         }
-        for(int i = 0; i < fd_count; i++) 
+        for (unsigned long i = 0; i < fds.size(); i++) 
         {
             if (fds[i].revents & POLLIN) 
             {
                 if (fds[i].fd == sockfd) 
                 {
-                    // If listener is ready to read, handle new connection
                     size = sizeof(distaddr);
                     newfd = accept(sockfd, &distaddr, &size);
                     if (newfd == -1)
                         ft_error("accept");
                     else
-                        add_to_pfds(&fds, newfd, &fd_count, &fd_size);
+                        add_fd(newfd, &fds);
                 }
-                else
+                else if (fds.size() >= 1)// est ce que avant d'envoyer il faut check si POLLOUT est up ?
                 {
                     int nbytes = recv(fds[i].fd, buff, sizeof(buff), 0);
                     int sender_fd = fds[i].fd;
                     if (nbytes <= 0) 
                     {
-                            // Got error or connection closed by client
-                            if (nbytes == 0) {
-                                // Connection closed
-                                printf("pollserver: socket %d hung up\n", sender_fd);
-                            } else {
-                                perror("recv");
-                            }
-
-                            close(fds[i].fd); // Bye!
-
-                            del_from_pfds(fds, i, &fd_count);
-
+                        if (nbytes == 0)
+                            printf("pollserver: socket %d hung up\n", sender_fd);
+                        else
+                            perror("recv");
+                        close(fds[i].fd);
+                        delete_one(&fds, fds[i]);
                     } 
                     else 
                     {
-                        // We got some good data from a client
-                        for(int j = 0; j < fd_count; j++)
+                        for(unsigned long j = 0; j < fds.size(); j++)
                         {
-                            // Send to everyone!
                             int dest_fd = fds[j].fd;
-                            // Except the listener and ourselves
                             if (dest_fd != sockfd && dest_fd != sender_fd)
                             {
                                 if (send(dest_fd, buff, nbytes, 0) == -1) 
-                                {
                                     perror("send");
-                                }
                             }
                         }
                     }
@@ -237,16 +173,6 @@ int main(int argc, char **argv)
             }
         }
     }
-  
-        // // on lit et on envoie une confirmation de reception
-        // read_ret = recv(newfd, buff, BUFFER_SIZE, 0);
-        // if (read_ret < 0)
-        //     break;
-        // send(newfd, "test\n", strlen("test\n"), MSG_NOSIGNAL);
-        // std::cout << buff << std::endl;
-        // if (i++ == 4)
-        //     break;
     close(sockfd);
-    free(fds);
     std::cout << "end" << std::endl;
 }
