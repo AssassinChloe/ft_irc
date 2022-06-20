2ae27d7cc25f09150f371bb9bf4890ff7c2c3358
vomercier <81368227+vomercier@users.noreply.github.com>
2022-06-20 16:41:12 +0200

    lien avec execCommand

--- Server.hpp
+++ Server.hpp
@@ -10,7 +10,8 @@ class Server
         std::string                 _hostname;
         struct addrinfo             _info;
         struct pollfd               _socket;
-        std::vector<struct pollfd>  _poll_fd;             
+        std::vector<struct pollfd>  _poll_fd;    
+        std::map<int, Client>     _clients;        
         
     public:
         Server(std::string port, std::string host);
@@ -21,6 +22,8 @@ class Server
         int using_poll();
         int parse_data(int i);
         int accept_client(int i);
+
+        const Client &getClient(int);
 
         void ft_split(std::vector<std::string> *tab, std::string str);
 };
