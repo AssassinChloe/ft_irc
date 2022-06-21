14a476b44165f8943ed73a218ea04ad6ee425ea8
Valerie MERCIER <vmercier@e3r4p1.42.fr>
2022-06-21 11:50:30 +0200

    integration du  ping

--- Command.cpp
+++ Command.cpp
@@ -70,6 +70,7 @@ void Command::execCommand()
 	switch (get_cmd_id(cmdType)){
 		case PING:
 			std::cout << "case ping" << std::endl;
+			Command::Ping();
 			break;
 		case NICK:
 			std::cout << "case nick" << std::endl;
@@ -80,13 +81,13 @@ void Command::execCommand()
 		case USER:
 			std::cout << "case user" << std::endl;
 			break;	
-		case OPER:
+		case OPER: 
 			std::cout << "case oper" << std::endl;
 			break;
 		case QUIT:
 			std::cout << "case quit" << std::endl;
 			break;
-		case CAP:
+		case CAP: // pas a gerer 
 			std::cout << "case cap" << std::endl;
 			break;
 		case PONG:
@@ -121,7 +122,7 @@ void welcome(int fd, std::string prefixe, std::string nickname)
     std::string hostinfo = prefixe + "002 " + nickname + " " + RPL_YOURHOST((std::string)SERVER_NAME, VERSION);
     std::string myinfo = prefixe + "003 " + nickname + " " + RPL_MYINFO((std::string)SERVER_NAME, VERSION, USER_MODE, CHAN_MODE);
     std::string creation = prefixe + "004 " + nickname + " " + RPL_CREATED((std::string)CREATION);
-    std::cout << "parse welcome" << std::endl;
+    std::cout << "parse welcome fd" << fd << std::endl;
     send(fd, welcome.c_str(), welcome.size(), 0);
     send(fd, hostinfo.c_str(), hostinfo.size(), 0);
     send(fd, myinfo.c_str(), myinfo.size(), 0);
--- PingPong.cpp
+++ PingPong.cpp
@@ -3,19 +3,26 @@
 #include "Server.hpp"
 #include <ctime>
 
-void PING(Command *command)
+void Command::Ping()
 {
-	if (command->getParameters().size() == 0)
+	if (this->getParameters().size() == 0)
 		std::cout << "bientot une reply 409" << std::endl;
 		// return command->reply(409);
+	int fd_client = this->getClient().getFd();
+	std::string buf = (this->getClient().getPrefixe() + "PONG :" + this->getParameters()[0]);
+	send(fd_client, buf.c_str(), buf.size(), 0);
+	// std::string test = "test";
+	// send(fd_client, test.c_str(), test.size(), 0);
+	// send(fd_client, (this->getClient().getPrefixe() + " PONG :" + this->getParameters()[0]).c_str(), (" PONG :" + this->getParameters()[0]).size(), 0);
+	std::cout << "fd: "<< fd_client << ", buf Ping: " << buf << std::endl;
 	// command->getClient().sendTo(command->getClient(), "PONG :" + command->getParameters()[0]);
 }
 
 
-void PONG(Command *command)
-{
-	if (command->getParameters().size() == 0)
-	std::cout << "bientot une reply 409" << std::endl;
-		// return command->reply(409);
-	command->getClient().setLastPing(std::time(0));
-}
+// void PONG(Command *command)
+// {
+// 	if (command->getParameters().size() == 0)
+// 	std::cout << "bientot une reply 409" << std::endl;
+// 		// return command->reply(409);
+// 	command->getClient().setLastPing(std::time(0));
+// }
