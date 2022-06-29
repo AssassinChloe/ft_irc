6aeecac17f5cc8f3e4709353ba11f6e5942d45dd
Valérie <vmercier@student.42.fr>
2022-06-29 19:18:34 +0200

    .

--- Privmsg.cpp
+++ Privmsg.cpp
@@ -12,11 +12,9 @@ for (i=0; i<nb_param; i++)
 
     if (parameters[i][0] == '#' || parameters[i][0] == '&' || parameters[i][0] == '+' || parameters[i][0] ==  '!')
     {
-        // std::cout << "parameter0: " << parameters[0] << std::endl;
-        // std::cout << "parameter1: " << parameters[1] << std::endl;
-        // std::cout << "parameters.size: " << parameters.size() << std::endl;
 
-        std::string message = this->client->getPrefixe() + "PRIVMSG " +  parameters[0] + " :" + this->getArgLine() + " \r\n";
+
+        std::string message = this->client->getPrefixe() + "PRIVMSG " +  parameters[i] + " :" + this->getArgLine() + " \r\n";
         std::cout << message << std::endl;
 
         int index = server->getChannelIndex(parameters[i]);
@@ -43,25 +41,7 @@ for (i=0; i<nb_param; i++)
                 std::cout <<"message individuel : " << message << std::endl;
             }
         }
-    }
-
-
-        // send_message(*this->client, message);
-
-        // std::cout << "parameter1: " << parameters[1] << std::endl;
-        
-        // std::string message = this->client->getPrefixe() + " PRIVMSG " +  parameters[0] + " ";
-        // int param_size = this->getParameters().size();
-        // for (int i=1; i<param_size; i++)
-        // {
-        //     message = message + parameters[i] +" ";
-        //     std::cout << "parameter[" << i << "]: " << parameters[i] << std::endl;
-        // }
-        // message = message + "\r\n" ;
-        // std::cout << message << std::endl;
-        // send_message(*this->client, message);
-        
-    
+    }     
 }
 }
 
