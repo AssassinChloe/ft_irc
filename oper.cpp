d8a7cc62771905a53310c7c8aeda4d708ad64586
cassassi <cassassi@student.42.fr>
2022-06-30 12:24:43 +0200

    this is hard

--- oper.cpp
+++ oper.cpp
@@ -2,5 +2,11 @@
 
 void Command::oper()
 {
+    if (this->parameters.size() < 2)
+    {
+        std::string message = "OPER :Not enough parameters\r\n";
+        send(this->client->getFd(), message.c_str(), message.size(), 0);
+        return ;
+    }
     
 }
