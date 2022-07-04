4157a1e192c564fc71dec411ae07e56105653128
Chloe ASSASSIN <cassassi@e1r1p11.42.fr>
2022-07-04 10:30:12 +0200

    no segfault is better

--- quit.cpp
+++ quit.cpp
@@ -2,22 +2,13 @@
 
 void Command::quit()
 {
-    std::cout << this->client->getNickname() << " quit the server" << std::endl;
-    if (this->client->getChanList().size() > 0)
+    unsigned long i;
+
+    for (i = 0; i < this->server->getPollFdList().size(); i++)
     {
-        Command command_line(*this->client, this->server, "JOIN 0");
-        command_line.execCommand();
-    }
-    close(this->client->getFd());
-    if (this->server->getClientList().size() > 0)
-        this->server->getClientList().erase(this->client->getFd());
-    std::vector<struct pollfd> tmp = this->server->getPollFdList();
-    for (std::vector<struct pollfd>::iterator it = tmp.begin(); it != tmp.end(); it++)
-    {
-        if ((*it).fd == this->client->getFd())
+        if (this->server->getPollFdList()[i].fd == this->client->getFd())
         {
-            this->server->getPollFdList().erase(it);
-            return ;
+            this->server->deleteClient(i);
         }
     }
 }
