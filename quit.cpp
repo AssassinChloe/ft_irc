546a03396b66d39951e859dbbcf85911b2407145
Chloe ASSASSIN <cassassi@e1r2p21.42.fr>
2022-07-07 14:33:46 +0200

    quit ok?

--- quit.cpp
+++ quit.cpp
@@ -2,13 +2,14 @@
 
 void Command::quit()
 {
-    // unsigned long i;
-
-    // for (i = 0; i < this->server->getPollFdList().size(); i++)
-    // {
-    //     if (this->server->getPollFdList()[i].fd == this->client->getFd())
-    //     {
-            this->server->deleteClient(this->client->getFd());
-        // }
-    // }
+    std::string message = this->client->getPrefixe() + "QUIT Quit: " + this->argLine + "\r\n";
+    if (this->client->getChanList().size() > 0)
+    {
+        std::map<std::string, std::string> tmp = this->client->getChanList();
+        for (std::map<std::string, std::string>::iterator it = tmp.begin(); it != tmp.end(); it++)
+        {
+            this->server->getChannel((*it).first).broadcast(message);
+        }
+    }
+    this->server->deleteClient(this->client->getFd());
 }
